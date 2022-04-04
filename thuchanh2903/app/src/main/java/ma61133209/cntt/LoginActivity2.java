d8425e5e065b6ae881152a5dcad93e0b0c42c1a0
HuynhNhatTruong0108 <kuda.2001.08.01@gmail.com>
2022-04-04 21:38:13 +0700

    baitap

---
+++ thuchanh2903/app/src/main/java/ma61133209/cntt/LoginActivity2.java
@@ -0,0 +1,33 @@
+package ma61133209.cntt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+
+    }
+    /*
+    protected void onclick(int requesCode, int resultCode, Intent data)
+    {
+        if (requesCode==8000)
+            if (resultCode == RESULT_OK)
+            {
+                String hoten = data.getStringExtra("ten");
+                int makhau = data.getIntExtra("NS", 123);
+                TextView tvHT = (TextView)findViewById(R.id.);
+
+            }
+    } */
+
+}
