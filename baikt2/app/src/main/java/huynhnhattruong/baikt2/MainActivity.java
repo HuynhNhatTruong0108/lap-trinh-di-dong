b7c0c2fa7c59182a6034a3952513ee3cedaf4c92
HuynhNhatTruong0108 <kuda.2001.08.01@gmail.com>
2022-04-11 11:19:45 +0700

    bai tap

--- baikt2/app/src/main/java/huynhnhattruong/baikt2/MainActivity.java
+++ baikt2/app/src/main/java/huynhnhattruong/baikt2/MainActivity.java
@@ -5,22 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
-    Button phay, cham, cach, xoa, gach, tick;
-    TextView Manhinh;
+    Button phay, cham, cach, xoa, gach, tick, bang;
+    TextView Manhinh, ketqua;
     String tinhtoan;
-    double ketqua;
+    double  kq, so1;
+
+    RadioButton R1, R2, R3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        double Rd1 = 0.13;
+        double Rd2 = 0.15;
+        double Rd3 = 0.15;
         anhxa();
+
         btn0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -96,9 +103,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Manhinh.setText("");
                 tinhtoan = "";
+
             }
         });
-       
+        bang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tinhtoan = ketqua.getText().toString();
+                ketqua.setText("");
+                if (R1.isChecked()){
+                    kq = 100 * Rd1;
+                }
+                else if (R2.isChecked()){
+                    kq = 100 * Rd2;
+                }
+                else  if (R3.isChecked()){
+                    kq = 100 * Rd3;
+                }
+                else return;
+                ketqua.setText("you tip will be $"+ kq + "" );
+            }
+        });
     }
     protected void anhxa()
     {
@@ -114,6 +139,13 @@ public class MainActivity extends AppCompatActivity {
         btn9 = findViewById(R.id.btn9);
         Manhinh = findViewById(R.id.manhinh);
         xoa = findViewById(R.id.dauxoa);
+        tick = findViewById(R.id.dautick);
+        R1 = findViewById(R.id.R1);
+        R2 = findViewById(R.id.R2);
+        R3 = findViewById(R.id.R3);
+        bang =findViewById(R.id.Bang);
+        ketqua =findViewById(R.id.ketqua);
+
     }
 
 }
